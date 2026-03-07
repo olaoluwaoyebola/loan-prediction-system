662875c375cd82b26f7c7cfae4b831cc91b40b0f
Olaoluwa Oyebola <olaoluwaoyebola@gmail.com>
2026-03-07 20:08:19 +0100

    feat: prediction model

--- backend/prediction.py
+++ backend/prediction.py
@@ -1,4 +1,11 @@
 import pickle
+import pandas as pd
 
-model = pickle.load(open("model/loan_model.pkl", "rb"))
-columns = pickle.load(open("model/loan_columns.pkl", "rb"))
+model = pickle.load(open("model/loan_model.pkl", "rb"))            # model
+columns = pickle.load(open("model/loan_columns.pkl", "rb"))        # training features
+
+def predict_loan(data):
+    """Run prediction with the model based on applicant data"""
+    input_df = pd.DataFrame([data], columns=columns)                # convert input data to df
+    prediction = model.predict(input_df)                            # run model prediction
+    return int(prediction[0])                                       # extract prediction value as int
